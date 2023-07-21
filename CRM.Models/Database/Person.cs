f366a0b11b1618ed0a8bc566f1ee4e5e659e4af5
Jakub Becker <jakub.becker@banqsoft.pl>
2023-07-21 17:46:54 +0200

    Add person and to do item models

---
+++ CRM.Models/Database/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace CRM.Models.Database
+{
+    public class Person
+    {
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public Role Role { get; set; }
+
+        public Person(string username, string hashedPassword, string? firstName, string? lastName, Role role)
+        {
+            Username = username;
+            HashedPassword = hashedPassword;
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+        }
+    }
+}
---
+++ CRM.Models/Database/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿namespace CRM.Models.Database
+{
+    public class ToDoItem
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Text { get; set; }
+        public int Progress { get; set; }
+        public Person AssignedTo { get; set; }
+        public Person CreatedBy { get; }
+
+        public ToDoItem(DateTime startTime, DateTime endTime, string text, int progress, Person assignedTo, Person createdBy)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            Text = text;
+            Progress = progress;
+            AssignedTo = assignedTo;
+            CreatedBy = createdBy;
+        }
+    }
+}
