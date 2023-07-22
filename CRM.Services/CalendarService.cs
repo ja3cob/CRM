e4b5fad90a5a5068b107c0608a1798d33f92371d
Jakub Becker <jakub.becker@banqsoft.pl>
2023-07-22 13:42:39 +0200

    Add dummy data

--- CRM.Services/CalendarService.cs
+++ CRM.Services/CalendarService.cs
@@ -1,4 +1,5 @@
 ﻿using CRM.Models.General;
+using CRM.Models.Database;
 
 namespace CRM.Services
 {
@@ -22,6 +23,26 @@ namespace CRM.Services
                 days.Add(new Day(tempDay));
                 tempDay = tempDay.AddDays(1);
             }
+
+            var testPerson = new Person("test", "test", null, null, Models.Role.User);
+            var startTime = new DateTime(days[4].CurrentDate.Year, days[4].CurrentDate.Month, days[4].CurrentDate.Day, 10, 0, 0);
+            var endTime = startTime.AddHours(3);
+
+            string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat, eros eget posuere venenatis, velit nisl ullamcorper tortor, non eleifend elit libero eu quam. Sed id facilisis nisi.";
+            days[4].ToDoItems = new List<ToDoItem>
+            {
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+            };
+            days[2].ToDoItems = new List<ToDoItem>
+            {
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+                new ToDoItem(startTime, endTime, lorem, 0, testPerson, testPerson),
+            };
 
             return new Month(days.ToArray(), month, year);
         }
