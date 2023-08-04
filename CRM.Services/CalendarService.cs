001f5bf346a4fda92351099b0dae02405e144f6b
Jakub Becker <jakub.becker@banqsoft.pl>
2023-08-04 19:13:26 +0200

    Move month generator to typeScript

--- CRM.Services/CalendarService.cs
+++ CRM.Services/CalendarService.cs
@@ -1,51 +1,6 @@
-﻿using CRM.Models.General;
-using CRM.Models.Database;
-
-namespace CRM.Services
+﻿namespace CRM.Services
 {
     public class CalendarService
     {
-        private const int DaysInCalendar = 7 * 6;
-        public static Month GenerateMonth(int year, int month)
-        {
-            var days = new List<Day>();
-
-            var tempDay = new DateOnly(year, month, 1);
-            days.Add(new Day(tempDay));
-
-            for (int i = -1; days[0].Date.DayOfWeek != DayOfWeek.Monday; i--)
-            {
-                days.Insert(0, new Day(tempDay.AddDays(i)));
-            }
-            tempDay = tempDay.AddDays(1);
-
-            while (days.Count < DaysInCalendar)
-            {
-                days.Add(new Day(tempDay));
-                tempDay = tempDay.AddDays(1);
-            }
-
-            string testPerson = "test";
-            var startTime = new TimeOnly(10, 0);
-            var endTime = startTime.AddHours(3);
-
-            string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat, eros eget posuere venenatis, velit nisl ullamcorper tortor, non eleifend elit libero eu quam. Sed id facilisis nisi.";
-            days[9].ToDoItems = new List<ToDoItem>
-            {
-                new ToDoItem(days[9].Date, startTime, endTime, lorem, 0, testPerson, testPerson),
-                new ToDoItem(days[9].Date, startTime.AddHours(1), endTime.AddHours(1), lorem, 10, testPerson, testPerson),
-                new ToDoItem(days[9].Date, startTime.AddHours(2), endTime.AddHours(2), lorem, 20, testPerson, testPerson),
-                new ToDoItem(days[9].Date, startTime.AddHours(3), endTime.AddHours(3), lorem, 30, testPerson, testPerson),
-            };
-            days[11].ToDoItems = new List<ToDoItem>
-            {
-                new ToDoItem(days[11].Date, startTime, endTime, lorem, 0, testPerson, testPerson),
-                new ToDoItem(days[11].Date, startTime.AddHours(1), endTime.AddHours(1), lorem, 10, testPerson, testPerson),
-                new ToDoItem(days[11].Date, startTime.AddHours(2), endTime.AddHours(2), lorem, 20, testPerson, testPerson),
-                new ToDoItem(days[11].Date, startTime.AddHours(3), endTime.AddHours(3), lorem, 30, testPerson, testPerson),
-            };
-
-            return new Month(new DateOnly(year, month, 1), days.ToArray());
-        }
     }
 }
---
+++ CRM/Scripts/MonthGenerator.ts
@@ -0,0 +1,46 @@
+﻿const daysInCalendar = 7 * 6;
+const daysInWeek = 7;
+const monday = 1;
+
+interface Date {
+    addDays(days: number): void;
+    toISOStringDate(): string;
+}
+Date.prototype.addDays = function (days) {
+    this.setDate(this.getDate() + days);
+}
+Date.prototype.toISOStringDate = function () {
+    const year = this.getFullYear();
+    var month = this.getMonth() + 1;
+    var day = this.getDate();
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    return year + '-' + month + '-' + day;
+}
+
+function generateMonth(year: number, month: number): Date[] {
+    var days = [];
+    const firstDay = new Date(year + "-" + month + "-1");
+    var tempDay = new Date(firstDay.getTime());
+    days.push(new Date(tempDay.getTime()));
+
+    while (days[0].getDay() != monday) {
+        tempDay.addDays(-1)
+        days.unshift(new Date(tempDay.getTime()));
+    }
+    tempDay = new Date(firstDay.getTime());
+    tempDay.addDays(1);
+
+    while (days.length < daysInCalendar) {
+        days.push(new Date(tempDay.getTime()));
+        tempDay.addDays(1);
+    }
+
+    return days;
+}
