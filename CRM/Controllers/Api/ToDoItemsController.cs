db675d38c3a44a36902febcc6e448d2b36a1f625
Jakub Becker <jakub.becker@banqsoft.pl>
2023-08-05 15:45:44 +0200

    Add to do items endpoint

---
+++ CRM/Controllers/Api/ToDoItemsController.cs
@@ -0,0 +1,26 @@
+﻿using CRM.Models.Database;
+using CRM.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoItemsController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ToDoItem>> GetForMonth([FromQuery] int? year, [FromQuery] int? month)
+        {
+            if (!year.HasValue || !month.HasValue)
+            {
+                year = DateTime.Now.Year;
+                month = DateTime.Now.Month;
+            }
+            if(month > 12 || month < 1)
+            {
+                return NotFound();
+            }
+            return Ok(CalendarService.GetToDoItems(year.Value, month.Value));
+        }
+    }
+}
