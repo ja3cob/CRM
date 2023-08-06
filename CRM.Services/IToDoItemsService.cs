eb4ae0e0f2520b6361460782a599753550bd6a69
ja3cob <jakubecker@gmail.com>
2023-08-06 21:32:18 +0200

    Change calendarService to toDoItemsService, add interface

---
+++ CRM.Services/IToDoItemsService.cs
@@ -0,0 +1,10 @@
+﻿using CRM.Models.Database;
+
+namespace CRM.Services
+{
+	public interface IToDoItemsService
+	{
+        public IEnumerable<ToDoItem> GetToDoItems(int year, int month);
+    }
+}
+
---
+++ CRM.Services/ToDoItemsService.cs
@@ -0,0 +1,20 @@
+﻿using CRM.Database;
+using CRM.Models.Database;
+
+namespace CRM.Services
+{
+	public class ToDoItemsService : IToDoItemsService
+	{
+		private readonly DatabaseService _databaseService;
+		public ToDoItemsService(DatabaseService databaseService)
+		{
+			_databaseService = databaseService;
+		}
+
+        public IEnumerable<ToDoItem> GetToDoItems(int year, int month)
+        {
+			return _databaseService.GetToDoItems(year, month);
+        }
+    }
+}
+
