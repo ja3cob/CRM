f50bfc66d7462111fabfacd2c20ff2d2b4ff7df2
Jakub Becker <jakub.becker@banqsoft.pl>
2023-08-12 12:28:11 +0200

    Add people controller

---
+++ CRM/Controllers/Api/PeopleController.cs
@@ -0,0 +1,22 @@
+﻿using CRM.Models.Database;
+using CRM.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers.Api;
+
+[ApiController]
+[Route("[controller]")]
+public class PeopleController : Controller
+{
+    private readonly PeopleService _peopleService;
+	public PeopleController(PeopleService peopleService)
+    {
+        _peopleService = peopleService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Person>> GetAllPeople()
+    {
+        return Ok(_peopleService.GetList());
+    }
+}
