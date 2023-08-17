47587d2d8ee21a1b71bc248ab8eaa3162464cbb2
Jakub Becker <jakub.becker@banqsoft.pl>
2023-08-17 20:41:46 +0200

    Add admin controller

---
+++ CRM/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers
+{
+    public class AdminController : Controller
+    {
+        public AdminController() 
+        {
+        }
+
+        [Route("/uzytkownicy")]
+        [HttpGet]
+        public IActionResult People()
+        {
+            return View("People");
+        }
+    }
+}
