7041b6f3ebad518e2b2e9d74f20c2a8959a88ac4
Jakub Becker <jakub.becker@banqsoft.pl>
2023-08-18 23:51:24 +0200

    Add function for adding person

--- CRM/Scripts/People.ts
+++ CRM/Scripts/People.ts
@@ -1,49 +1,49 @@
 ﻿function addPersonToList(person: Person) {
-            const row = document.createElement("div");
-            row.classList.add("people-row");
+    const row = document.createElement("div");
+    row.classList.add("people-row");
 
-            const username = document.createElement("div");
-            username.classList.add("row-username");
-            username.innerHTML = person.username;
-            row.appendChild(username);
+    const username = document.createElement("div");
+    username.classList.add("row-username");
+    username.innerHTML = person.username;
+    row.appendChild(username);
 
-            const firstName = document.createElement("div");
-            firstName.classList.add("row-firstname");
-            firstName.innerHTML = person.firstName;
-            row.appendChild(firstName);
+    const firstName = document.createElement("div");
+    firstName.classList.add("row-firstname");
+    firstName.innerHTML = person.firstName;
+    row.appendChild(firstName);
 
-            const lastName = document.createElement("div");
-            lastName.classList.add("row-lastname");
-            lastName.innerHTML = person.lastName;
-            row.appendChild(lastName);
+    const lastName = document.createElement("div");
+    lastName.classList.add("row-lastname");
+    lastName.innerHTML = person.lastName;
+    row.appendChild(lastName);
 
-            const role = document.createElement("div");
-            role.classList.add("row-role");
-            role.innerHTML = person.role.toString();
-            row.appendChild(role);
+    const role = document.createElement("div");
+    role.classList.add("row-role");
+    role.innerHTML = person.role.toString();
+    row.appendChild(role);
 
-            const buttons = document.createElement("div");
-            buttons.classList.add("row-buttons");
+    const buttons = document.createElement("div");
+    buttons.classList.add("row-buttons");
 
-            const editButton = document.createElement("button");
-            editButton.classList.add("btn-edit");
-            editButton.classList.add("btn");
-            editButton.classList.add("btn--outline");
-            editButton.setAttribute("onclick", `showPersonEditor(${JSON.stringify(person)})`);
-            editButton.innerHTML = "Edytuj";
-            buttons.appendChild(editButton);
+    const editButton = document.createElement("button");
+    editButton.classList.add("btn-edit");
+    editButton.classList.add("btn");
+    editButton.classList.add("btn--outline");
+    editButton.setAttribute("onclick", `showPersonEditor(${JSON.stringify(person)})`);
+    editButton.innerHTML = "Edytuj";
+    buttons.appendChild(editButton);
 
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("btn-delete");
-            deleteButton.classList.add("btn");
-            deleteButton.classList.add("btn--primary");
-            deleteButton.innerHTML = "Usuń";
-            buttons.appendChild(deleteButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn-delete");
+    deleteButton.classList.add("btn");
+    deleteButton.classList.add("btn--primary");
+    deleteButton.innerHTML = "Usuń";
+    buttons.appendChild(deleteButton);
 
-            row.appendChild(buttons);
+    row.appendChild(buttons);
 
 
-            document.querySelector(".people-list").appendChild(row);
+    document.querySelector(".people-list").appendChild(row);
 }
 
 function loadUsers() {
@@ -108,6 +108,24 @@ function showPersonEditor(person: Person) {
 function hidePersonEditor() {
     document.querySelector(".person-editor-container").classList.add("hidden");
 }
+
+function addPerson() {
+    const inputs = getPersonInputs();
+    const person: Person = {
+        username: inputs.username.value,
+        firstName: inputs.firstName.value,
+        lastName: inputs.lastName.value,
+        role: Number(inputs.role.value)
+    };
+
+    postData("/people", person).then(response => {
+        if (response.ok) {
+            addPersonToList(person);
+            hidePersonEditor();
+        }
+    });
+}
+
 function updatePersonData(username: string, person: Person) {
     const rows = document.querySelectorAll(".people-row");
     rows.forEach(row => {
