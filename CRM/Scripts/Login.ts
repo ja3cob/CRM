01cc95cd644dc5d247b3a057a2266e4ed1a80a2b
ja3cob <jakubecker@gmail.com>
2023-08-27 23:21:29 +0200

    Add script for sending login request

---
+++ CRM/Scripts/Login.ts
@@ -0,0 +1,14 @@
+function getLoginInputs() {
+    return {
+        username: (<HTMLInputElement>document.querySelector("input[name=username]")).value,
+        password: (<HTMLInputElement>document.querySelector("input[name=password]")).value
+    }
+}
+function login() {
+    const inputs = getLoginInputs();
+    postData(urls.apiLogin, { Username: inputs.username, Password: inputs.password }).then(response => {
+        if (response.ok) {
+            window.location.replace("/");
+        }
+    });
+}
