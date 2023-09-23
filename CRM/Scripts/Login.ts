484355e02a5b75eff9876a4577aaa8a960d7149d
ja3cob <jakubecker@gmail.com>
2023-09-23 15:30:37 +0200

    Login on enter

--- CRM/Scripts/Login.ts
+++ CRM/Scripts/Login.ts
@@ -1,3 +1,9 @@
+document.querySelector("input[name=password]").addEventListener("keypress", e => {
+    if ((<KeyboardEvent>e).key == "Enter") {
+        login();
+    }
+})
+
 function getLoginInputs() {
     return {
         username: (<HTMLInputElement>document.querySelector("input[name=username]")).value,
